b1af7046e91a87f6916f4e3da329741f9833656e
Xun Chen <xunchen369@gmail.com>
2021-03-11 17:33:42 +0800

    added routes fixed html

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
+
+// https://github.com/ReactTraining/react-router/blob/dev/docs/installation/getting-started.md
+// https://github.com/ReactTraining/react-router/blob/dev/docs/advanced-guides/migrating-5-to-6.md#use-navigate-instead-of-history
 
 export default function RouteMap() {
   return (
     <BrowserRouter>
-      <Home />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Navigate to="/home" replace />
+      </Routes>
     </BrowserRouter>
   );
 }
